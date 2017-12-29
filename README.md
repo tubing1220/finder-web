d5c8384273d632b919d40b26f176bba69066c621
tubing <tubing1220@163.com>
2017-12-29 12:20:40 +0800

    update slim framework

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # finder-web
+
+1: composer require slim/slim "^3.9.2"
+2: if apache server create .htaccess file
+3: create index.php
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello, world");
+
+    return $response;
+});
+$app->run();
